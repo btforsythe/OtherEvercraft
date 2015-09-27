020c44828e8ebf8eb8aa3ef1c94a19cd5a2057b1
scoobycoder <scoobycoder@gmail.com>
2015-09-27 13:10:25 -0400

    removing extra mock because I don't need it yet

--- test/character/LevelingTest.java
+++ test/character/LevelingTest.java
@@ -2,18 +2,12 @@ package character;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 
 public class LevelingTest {
-
-	@Mock
-	EvercraftCharacter character;
-	
 	private Level underTest;
 	
 	@Before
